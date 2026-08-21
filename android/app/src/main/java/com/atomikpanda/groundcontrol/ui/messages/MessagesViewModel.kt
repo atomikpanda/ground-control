56de216c32f02ad7bc299a63b69ae548c14c1d03
Bailey Seymour <bailey@baileyseymour.com>
2026-08-21 15:57:35 +0000

    fix: restore message coroutine imports

--- android/app/src/main/java/com/atomikpanda/groundcontrol/ui/messages/MessagesViewModel.kt
+++ android/app/src/main/java/com/atomikpanda/groundcontrol/ui/messages/MessagesViewModel.kt
@@ -8,11 +8,13 @@ import com.atomikpanda.groundcontrol.data.findByConnectionId
 import com.atomikpanda.groundcontrol.data.dto.ThreadSummary
 import com.atomikpanda.groundcontrol.data.dto.WorkItemSummary
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
